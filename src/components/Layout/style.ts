c9dcaafe7c26aebe12d995afc06dfdbed8bb11fd
Misha Kompaniec <mikokompaniec@gmail.com>
2023-01-23 17:49:38 +0200

    changed style

--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
   max-width: 980px;
-  height: 100vh;
   margin: 0 auto;
   padding-bottom: 90px;
-  background-color: #f7f7f7;
 `;
