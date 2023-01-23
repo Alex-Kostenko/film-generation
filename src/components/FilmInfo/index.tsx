7268ba4312af7fe37d70dc7253bb5aaec6515faf
Misha Kompaniec <mikokompaniec@gmail.com>
2023-01-23 09:31:35 +0200

    changed style FilmInfo

--- src/components/FilmInfo/index.tsx
+++ src/components/FilmInfo/index.tsx
@@ -36,19 +36,36 @@ const FilmInfo: FC<IFilmInfoProps> = ({
 }) => {
   return (
     <div className={style.descriptionContainer}>
-      <div className={style.descriptionNameContainer}>
-        {DESCRIPTION_ITEM.map((item) => (
-          <span className={style.descriptionName}>{item}:</span>
-        ))}
-      </div>
-      <div className={style.descriptionTextContainer}>
+      <div className={style.descriptionItem}>
+        <span className={style.descriptionName}>name:</span>
         <span className={style.descriptionText}>{name}</span>
+      </div>
+      <div className={style.descriptionItem}>
+        <span className={style.descriptionName}>year:</span>
         <span className={style.descriptionText}>{year}</span>
+      </div>
+      <div className={style.descriptionItem}>
+        <span className={style.descriptionName}>country:</span>
         <span className={style.descriptionText}>{country}</span>
+      </div>
+      <div className={style.descriptionItem}>
+        <span className={style.descriptionName}>genre:</span>
         <span className={style.descriptionText}>{genre}</span>
+      </div>
+      <div className={style.descriptionItem}>
+        <span className={style.descriptionName}>time:</span>
         <span className={style.descriptionText}>{time}</span>
+      </div>
+      <div className={style.descriptionItem}>
+        <span className={style.descriptionName}>voice acting:</span>
         <span className={style.descriptionText}>{voiceActing}</span>
+      </div>
+      <div className={style.descriptionItem}>
+        <span className={style.descriptionName}>director:</span>
         <span className={style.descriptionText}>{director}</span>
+      </div>
+      <div className={style.descriptionItem}>
+        <span className={style.descriptionName}>starring:</span>
         <span className={style.descriptionText}>{starring}</span>
       </div>
     </div>
