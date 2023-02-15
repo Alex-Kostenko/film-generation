612e6c4b2d0e8693e44bf37985d0dd41a00b2b23
Ivan5ik <lacoste545438@gmail.com>
2023-02-15 10:38:05 +0200

    1

--- src/pages/style.ts
+++ src/pages/style.ts
@@ -166,25 +166,25 @@ export const Root = styled.div`
   }
 `;
 
-export const ReactSlick = styled.div`
-  position: relative;
-  z-index: 2;
-  display: block;
-  height: 450px;
-  width: 100%;
-  left: 0px;
-  top: 0px;
+// export const ReactSlick = styled.div`
+//   position: relative;
+//   z-index: 2;
+//   display: block;
+//   height: 450px;
+//   width: 100%;
+//   left: 0px;
+//   top: 0px;
 
-  @media (max-width: 1340px) {
-    height: 400px;
-  }
-  @media (max-width: 900px) {
-    height: 350px;
-  }
-  @media (max-width: 600px) {
-    height: 250px;
-  }
-`;
+//   @media (max-width: 1340px) {
+//     height: 400px;
+//   }
+//   @media (max-width: 900px) {
+//     height: 350px;
+//   }
+//   @media (max-width: 600px) {
+//     height: 250px;
+//   }
+// `;
 
 export const BurgerHeader = styled.div`
   display: none;
