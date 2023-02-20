a424ac6ca89749ed4e558767994565a3454a6419
Ivan5ik <lacoste545438@gmail.com>
2023-02-20 18:06:00 +0200

    fix yarn lint

--- src/pages/movieList/index.tsx
+++ src/pages/movieList/index.tsx
@@ -1,10 +1,11 @@
 import { useRouter } from 'next/router';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { FC, useEffect, useRef, useState } from 'react';
 import ReactPaginate from 'react-paginate';
 
+import queryMovie from '@/Services/queryMovies';
 import BackBtn from '@/components/BackBtn';
-import { IMovieListProps, IMovie } from '@/interfaces';
+import { IMovieListProps } from '@/interfaces';
 import {
   SearchCriteria,
   CardComponent,
@@ -17,9 +18,7 @@ import {
 
 import Reload from '../../../public/reload.svg';
 
-import queryMovie from '@/Services/queryMovies';
-
-const MovieList: FC<IMovieListProps> = ({ MOVIES }) => {
+const MovieList: FC<IMovieListProps> = () => {
   const router = useRouter();
   const { category, filmByCompany } = router.query;
 
@@ -130,65 +129,5 @@ const MovieList: FC<IMovieListProps> = ({ MOVIES }) => {
     </Root>
   );
 };
-
-export async function getServerSideProps({ locale }: any) {
-  const MOVIES = [
-    {
-      engTitle: 'The Green Mile',
-      rusTitle: 'Зеленая миля',
-      lables: ['detective', 'dramma'],
-      date: '22.12.1998',
-      id: 1,
-      description:
-        "A supernatural tale set on death row in a Southern prison, where gentle giant John Coffey possesses the mysterious power to heal people's ailments. When the cell block's head guard, Paul Edgecomb, recognizes Coffey's miraculous gift, he tries desperately to help stave off the condemned man's execution.A supernatural tale set on death row in a Southern prison, where gentle giant John Coffey possesses the mysterious power to heal people's ailments. When the cell block's head guard, Paul Edgecomb, recognizes Coffey's miraculous gift, he tries desperately to help stave off the condemned man's execution.imprisoned for six months for robbery, but Philippe does not care about Driss's past.execution.A supernatural tale set on death row in a Southern prison, where gentle giant John Coffey possesses the mysterious power to heal people's ailments. When the cell block's head guard, Paul Edgecomb, recognizes Coffey's miraculous gift, he tries desperately to help stave off the condemned man's execution.imprisoned for six months for robbery, but Philippe does not care about Driss's past.",
-      img: 'https://data-vyhoda.ru/wp-content/uploads/2020/03/2020-03-05_20-02-35.jpg',
-    },
-    {
-      engTitle: 'Hachiko',
-      rusTitle: 'Хатико',
-      lables: ['dramma', 'horor'],
-      date: '21.12.2008',
-      id: 2,
-      description:
-        "Commuting by train, music professor Parker Wilson finds an Akita puppy, whose cage broke unnoticed during shipping, leaving his destination unknown, and since the station can't care for it and the dog catcher warns even such cute ones may not be adopted in the two weeks allowed, he kindly takes it home. His bossy, jealous wife Cate initially makes Parker swear it won't stay, but by the time its' clear nobody will claim him and an adoption candidate is found, she agrees to keep the dog, who won over their daughter Andy and her fiance Michael at first sight. Parker's Japanese college friend Ken inspires naming the pup Hachi(ko), and is pleasantly surprised when Parker successfully tackles the challenge to get it to fetch, which Akitas don't usually do. Hachi makes a habit of waiting for his equally doting master at the station every evening, but after a cardiac crisis, Parker dies. Hachi refuses to accept this, being moved to Michael's home as Cate moves out, waiting for a master who can never come home again, by now collectively adopted by sympathizing town-folk. The story is told in flashbacks as class assignment 'my hero' by Michael's teen son Robbie, who also gets an Akita puppy.",
-      img: 'https://m.media-amazon.com/images/M/MV5BM2NmYjU4ODEtZjhjYi00MGNiLTg3NGMtZjUxNGIzOWZhYmRkXkEyXkFqcGdeQXVyMDgyNjA5MA@@._V1_.jpg',
-    },
-    {
-      engTitle: 'Avatar',
-      rusTitle: 'Аватар',
-      lables: ['dramma', 'adventure'],
-      date: '21.12.2012',
-      id: 3,
-      description:
-        "The next day when Driss returns, Philippe's aide Yvonne greets him, telling him he has the job on a trial basis. Despite being uninterested and inexperienced, he does well caring for Philippe, albeit using unconventional methods. Driss learns the extent of his employer’s disability, aiding Philippe in every aspect of his life. A friend of Philippe's reveals that Driss was imprisoned for six months for robbery, but Philippe does not care about Driss's past. As Driss is the only one who does not treat Philippe with pity, he will not fire Driss as long as he does his job properly.Philippe explains his disability resulted from a paragliding incident and that his wife died without bearing children. Gradually, Driss helps him to organise his private life, including disciplining his precocious adopted daughter Elisa. Driss discovers modern art, opera and starts painting. For Philippe's birthday, a private classical music concert is performed in his living room.",
-      img: 'https://cdn.theatlantic.com/thumbor/0jPGepNCkAs1qO4RS686VP5BGTE=/0x0:4800x2700/1600x900/media/img/mt/2022/09/avatar_rerelease_2/original.jpg',
-    },
-    {
-      engTitle: 'Forrest Gump',
-      rusTitle: 'Форест Гамп',
-      lables: ['dramma', 'history'],
-      date: '21.1MOVIES2.2005',
-      id: 4,
-      description:
-        "A supernatural tale set on death row in a Southern prison, where gentle giant John Coffey possesses the mysterious power to heal people's ailments. When the cell block's head guard, Paul Edgecomb, recognizes Coffey's miraculous gift, he tries desperately to help stave off the condemned man's execution.imprisoned for six months for robbery, but Philippe does not care about Driss's past. As Driss is the only one who does not treat Philippe with pity, he will not fire Driss as long as he does his job properly.Philippe explains his disability resulted from a paragliding incident and that his wife died without bearing children. Gradually, Driss helps him to organise his private life, including disciplining his precocious adopted daughter Elisa. Driss discovers modern art, opera and starts painting. For Philippe's birthday, a private classical music concert is performed in his living room.",
-      img: 'https://www.hollywoodreporter.com/wp-content/uploads/2019/06/forrest_gump-photofest_still_2-h_2019.jpg?w=1024',
-    },
-    {
-      engTitle: 'Intouchables',
-      rusTitle: '1+1',
-      lables: ['dramma', 'comedy'],
-      date: '21.12.2015',
-      id: 5,
-      description:
-        "The next day when Driss returns, Philippe's aide Yvonne greets him, telling him he has the job on a trial basis. Despite being uninterested and inexperienced, he does well caring for Philippe, albeit using unconventional methods. Driss learns the extent of his employer’s disability, aiding Philippe in every aspect of his life. A friend of Philippe's reveals that Driss was imprisoned for six months for robbery, but Philippe does not care about Driss's past. As Driss is the only one who does not treat Philippe with pity, he will not fire Driss as long as he does his job properly.Philippe explains his disability resulted from a paragliding incident and that his wife died without bearing children. Gradually, Driss helps him to organise his private life, including disciplining his precocious adopted daughter Elisa. Driss discovers modern art, opera and starts painting. For Philippe's birthday, a private classical music concert is performed in his living room.",
-    },
-  ];
-  return {
-    props: {
-      ...(await serverSideTranslations(locale)),
-      MOVIES,
-    },
-  };
-}
 
 export default MovieList;
