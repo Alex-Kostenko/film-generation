458df1d267582efbb397090009c77b5fe7de652d
Misha Kompaniec <mikokompaniec@gmail.com>
2023-02-27 12:07:45 +0200

    add tag

--- src/components/SliderText/index.tsx
+++ src/components/SliderText/index.tsx
@@ -4,8 +4,16 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { FC } from 'react';
 
 import { ISliderTextProps } from '@/interfaces';
+import { Genres } from '@/utils/genres';
 
-import { Description, Container, Btn, Title } from './style';
+import {
+  TagComponent,
+  TagContainer,
+  Description,
+  Container,
+  Btn,
+  Title,
+} from './style';
 
 const SliderText: FC<ISliderTextProps> = ({ movieData }) => {
   const { t } = useTranslation();
@@ -19,6 +27,12 @@ const SliderText: FC<ISliderTextProps> = ({ movieData }) => {
     <>
       <Container>
         <Title>{movieData.title}</Title>
+        <TagContainer>
+          {movieData.genre_ids.map((movie) => (
+            <TagComponent className="tag-small" label={Genres[movie]} />
+          ))}
+        </TagContainer>
+
         <Description>{movieData.overview}</Description>
         <Btn label={t('main.go')} onClick={() => redirect(movieData.id)}></Btn>
       </Container>
