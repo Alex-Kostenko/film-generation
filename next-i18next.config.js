aa295a170505777c524c49c500d3c239f7c15540
Misha Kompaniec <mikokompaniec@gmail.com>
2023-03-13 15:24:26 +0200

    fix bug i18next setting

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     locales: ['ua', 'en', 'ru'],
     localeDetection: false,
     defaultLocale: 'en'
   },
+  localePath: path.resolve('public/locales'),
 } 
