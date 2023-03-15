819322d02f463cec17cf9a4b334ebffe2cc8c20f
Misha Kompaniec <mikokompaniec@gmail.com>
2023-03-15 12:06:18 +0200

    change type

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -89,7 +89,7 @@ export interface IName {
 }
 
 export interface ISelectedFilms {
-  [index: string]: string;
+  [index: string]: string | undefined;
 }
 export interface IYearRange {
   startYear: number;
