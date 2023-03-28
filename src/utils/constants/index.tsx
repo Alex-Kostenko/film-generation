4b47ff0c7fa1794ec1280ee5b8f63dcdfc8a48d1
Misha Kompaniec <mikokompaniec@gmail.com>
2023-03-28 17:57:34 +0300

    bug  fix img

--- src/utils/constants/index.tsx
+++ src/utils/constants/index.tsx
@@ -12,3 +12,6 @@ export const settingsSlider = {
   autoplay: false,
   autoplaySpeed: 5000,
 };
+
+export const srcNoImage =
+  'https://media.istockphoto.com/id/1357365823/vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo.jpg?s=612x612&w=0&k=20&c=PM_optEhHBTZkuJQLlCjLz-v3zzxp-1mpNQZsdjrbns=';
